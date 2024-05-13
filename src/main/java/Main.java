b1236b62bee403357818aa55fc7c474f00ba8799
rigo0809 <169068411+rigo0809@users.noreply.github.com>
2024-05-13 23:48:15 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");    
+  System.out.println("  ");
+     System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!");
+     System.out.println("! Relato Sobre os Cursos !");
+     System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!");
+     System.out.println("  ");
+     System.out.println("Meu nome é Isabelle, estou no 1° ano do Ensino Medio, Cursando Tecnico de Desenvolvimento e Analise de Sistemas Em uma das disciplinas do curso, estamos estudando lógica computacional, assunto sobre o qual gostaria de conversar.");
+     System.out.println("  ");
+    System.out.println("No início do ano, abordamos tabelas de verdade e fizemos muitos exercícios para praticar, o que ajudou bastante na fixação do conteúdo. No final do primeiro trimestre, começamos a estudar algoritmos, o que foi muito instrutivo para entender como proceder ao programar.");
+       System.out.println("  ");
+System.out.println("Agora, no início do segundo trimestre, começamos a aprender sobre programação propriamente dita. Estamos aprendendo a escrever códigos de forma fácil e rápida, e isso tem contribuído para o nosso desenvolvimento na área.");
+     System.out.println("  ");
+  System.out.println("Estou gostando muito do curso e sinto que estou aprendendo cada vez mais sobre a técnica de programação.");
+     System.out.println("  ");
+     System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+  }
+}
